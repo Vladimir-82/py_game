32bb9d5fee4bfe03dbc0872eef71c039e192ddbe
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-10-15 15:37:19 +0300

    move right and left

--- ex1.py
+++ ex1.py
@@ -4,7 +4,7 @@ https://younglinux.info/pygame/key
 
 import pygame
 
-FPS = 10
+FPS = 30
 W, H = 800, 500
 
 pygame.init()
@@ -13,7 +13,15 @@ clock = pygame.time.Clock()
 
 v = 0
 h = W // 2
+
+RIGHT = "to the right"
+LEFT = "to the left"
+STOP = "stop"
+
 play = True
+
+motion = STOP
+
 
 while play:
 
@@ -24,9 +32,12 @@ while play:
             play = False
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_LEFT:
-                h -= 20
+                motion = LEFT
             elif i.key == pygame.K_RIGHT:
-                h += 20
+                motion = RIGHT
+        elif i.type == pygame.KEYUP:
+            if i.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                motion = STOP
 
 
     pygame.display.update()
@@ -36,5 +47,10 @@ while play:
     v += 30
     if v == 500:
         break
+
+    if motion == LEFT:
+        h -= 20
+    elif motion == RIGHT:
+        h += 20
 
     clock.tick(FPS)
