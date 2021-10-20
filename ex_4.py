2c1b1f61df42404acdde4026a7f7424b1c86f506
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-10-20 16:48:08 +0300

    score produced

--- ex_4.py
+++ ex_4.py
@@ -6,6 +6,8 @@ import pygame
 
 FPS = 20
 W, H = 500, 900
+RES = 900, 950
+
 R = 50
 
 level_50 = H
@@ -13,9 +15,20 @@ level_150 = H
 level_250 = H
 level_350 = H
 level_450 = H
+levels = [level_50, level_150, level_250, level_350, level_450]
 
 pygame.init()
-sc = pygame.display.set_mode((W, H))
+sc = pygame.display.set_mode(RES)
+sc.fill((200, 200, 200))
+game_sc = pygame.Surface((W, H))
+
+score = 0
+
+main_font = pygame.font.Font('font/font.ttf', 25)
+font = pygame.font.Font('font/font.ttf', 15)
+title_tetris = main_font.render("Tetris for Downs", True, pygame.Color('darkorange'))
+score_show = main_font.render("SCORE", True, pygame.Color('darkorange'))
+
 clock = pygame.time.Clock()
 
 v = 0
@@ -27,15 +40,19 @@ LEFT = "to the left"
 STOP = "stop"
 DOWN = 'down'
 
+RUNNING, PAUSE = 0, 1
+state = RUNNING
+
+
+
 play = True
 animation = []
 motion = STOP
 
 
 while play:
-
     clock.tick(FPS)
-
+    sc.blit(game_sc, (0, 0))
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             play = False
@@ -56,13 +73,13 @@ while play:
 
 
     pygame.display.update()
-    sc.fill((0, 0, 0))
+    game_sc.fill((0, 0, 0))
     if animation:
         for anim in animation:
-            pygame.draw.circle(sc, (255, 0, 0), anim, R)
+            pygame.draw.circle(game_sc, (255, 0, 0), anim, R)
 
 
-    pygame.draw.circle(sc, (100, 10, 200), (h, v), R)
+    pygame.draw.circle(game_sc, (100, 10, 200), (h, v), R)
     v += 5
 
 
@@ -84,40 +101,51 @@ while play:
 
 # не получается стройка
 
-    if v >= (level_50 - R) or v >= (level_150 - R) or v >= (level_250 - R) or v >= (level_350 - R) or v >= (level_450 - R):
-        if h == 50:
-            animation.append((h, level_50 - R))
-            level_50 = level_50 - 2 * R
-
-        elif h == 150:
-            animation.append((h, level_150 - R))
-            level_150 = level_150 - 2 * R
-
-        elif h == 250:
-            animation.append((h, level_250 - R))
-            level_250 = level_250 - 2 * R
-
-        elif h == 350:
-            animation.append((h, level_350 - R))
-            level_350 = level_350 - 2 * R
-
-        elif h == 450:
-            animation.append((h, level_450 - R))
-            level_450 = level_450 - 2 * R
+    if v >= (level_50 - R) and h == 50:
+        animation.append([h, level_50 - R])
+        level_50 = level_50 - 2 * R
+        v = 0
+    elif v >= (level_150 - R) and h == 150:
+        animation.append([h, level_150 - R])
+        level_150 = level_150 - 2 * R
+        v = 0
+    elif v >= (level_250 - R) and h == 250:
+        animation.append([h, level_250 - R])
+        level_250 = level_250 - 2 * R
+        v = 0
+    elif v >= (level_350 - R) and h == 350:
+        animation.append([h, level_350 - R])
+        level_350 = level_350 - 2 * R
+        v = 0
+    elif v >= (level_450 - R) and h == 450:
+        animation.append([h, level_450 - R])
+        level_450 = level_450 - 2 * R
         v = 0
 
-        if (50, 850) in animation and (150, 850) in animation and (250, 850) in animation and (350, 850) in animation and (350, 850) in animation:
-
-            animation.remove((50, 850))
-            animation.remove((150, 850))
-            animation.remove((250, 850))
-            animation.remove((350, 850))
-            animation.remove((450, 850))
 
 
 
-    print(animation)
+    if [50, 850] in animation and [150, 850] in animation and [250, 850] in animation and [350, 850] in animation and [450, 850] in animation:
 
+        animation.remove([50, 850])
+        animation.remove([150, 850])
+        animation.remove([250, 850])
+        animation.remove([350, 850])
+        animation.remove([450, 850])
+
+        for i in animation:
+            i[1] = i[1] + 100
+
+        level_50 = level_50 + 2 * R
+        level_150 = level_150 + 2 * R
+        level_250 = level_250 + 2 * R
+        level_350 = level_350 + 2 * R
+        level_450 = level_450 + 2 * R
+
+        score += 100
+
+    sc.blit(title_tetris, (570, 10))
+    sc.blit(score_show, (570, 50))
 
     clock.tick(FPS)
 
