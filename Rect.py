ceefdfc9586a8edf4851132788db4636dde9246f
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-10-26 13:46:05 +0300

    Rect complete

--- Rect.py
+++ Rect.py
@@ -1,57 +1,79 @@
 '''
 https://younglinux.info/pygame/rect
 '''
-from random import choice
+from random import randint
 import pygame as pg
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-collors =[BLACK, RED, GREEN, BLUE]
+
 FPS = 20
 W, H = 800, 800
 
-R = 0.8 * W
+R = W
 
-pg.init()
+# pg.init()
 sc = pg.display.set_mode((W, H))
 sc.fill((255, 255, 255))
 
 serf_1 = pg.Surface((W//2, H//2))
 serf_2 = pg.Surface((W//2, H//2))
 
-col_1 = choice(collors)
+serf_1.fill(BLACK)
+serf_2.fill(BLACK)
 
+for x, y in (0, 0), (W // 2, H // 2):
+    sc.blit(serf_1, serf_1.get_rect(topleft=(x, y)))
+for x, y in (W // 2, 0), (0, H // 2):
+    sc.blit(serf_2, serf_2.get_rect(topleft=(x, y)))
 
 clock = pg.time.Clock()
 
-
-
 pg.display.update()
-# surf.set_alpha(200)
-# cir = pg.draw.circle(sc, WHITE, (W//2, H//2), R)
+first_click = 'the first click'
+second_click = 'the second click'
+stop = 'stop'
 
+event = stop
 play = True
 while play:
     clock.tick(FPS)
-
     for i in pg.event.get():
-        if i.type == pg.K_SPACE:
+        if i.type == pg.QUIT:
             play = False
+
         elif i.type == pg.KEYDOWN:
-            if i.key == pg.K_F1:
-                col_1 = choice(collors)
-                serf_1.fill(col_1)
+            if i.key == pg.K_LEFT:
+                event = first_click
+
+            elif i.key == pg.K_RIGHT:
+                event = second_click
+
+            elif i.key == pg.K_DOWN:
+                serf_1.fill(BLACK)
+                serf_2.fill(BLACK)
                 for x, y in (0, 0), (W // 2, H // 2):
                     sc.blit(serf_1, serf_1.get_rect(topleft=(x, y)))
-
-            elif i.key == pg.K_F2:
-                col_2 = choice(collors)
-                while col_2 == col_1:
-                    col_2 = choice(collors)
-                serf_2.fill(col_2)
                 for x, y in (W // 2, 0), (0, H // 2):
                     sc.blit(serf_2, serf_2.get_rect(topleft=(x, y)))
-            elif i.key == pg.K_KP0:
-                pass
+            elif i.key == pg.K_UP:
+                play = False
+
+        elif i.type == pg.KEYUP:
+            if i.key in [pg.K_LEFT, pg.K_RIGHT]:
+                event = stop
+
+    if event == first_click:
+
+        serf_1.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+        for x, y in (0, 0), (W // 2, H // 2):
+            sc.blit(serf_1, serf_1.get_rect(topleft=(x, y)))
+
+    elif event == second_click:
+        serf_2.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+        for x, y in (W // 2, 0), (0, H // 2):
+            sc.blit(serf_2, serf_2.get_rect(topleft=(x, y)))
+    cir = pg.draw.circle(sc, WHITE, (W // 2, H // 2), R, R // 2 + 10)
+    pg.display.update()
+    clock.tick(FPS)
+
+
