2b085670292e027fd4a7141f9843b4b05d3bfc17
vladimir <grun_gespenst@tut.by>
2021-11-05 21:30:50 +0300

    snake does not grown

--- snake.py
+++ snake.py
@@ -71,11 +71,17 @@ class Head(pg.sprite.Sprite):
 
 
 class Body(pg.sprite.Sprite):
-    def __init__(self, pos, surf, group):
+    def __init__(self, x, y,  surf, group):
         pg.sprite.Sprite.__init__(self)
-        self.image = surf
-        self.rect = self.image.get_rect(center=pos)
+        self.image = pg.image.load(surf).convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+        self.x = x
+        self.y = y
         self.add(group)
+
+    def to_left(self):
+        self.x -= 2
+        return self.x, self.y
 
 class Apple(pg.sprite.Sprite):
     def __init__(self, x, y, surf, group):
@@ -102,7 +108,6 @@ heads = pg.sprite.Group()
 
 apples = pg.sprite.Group()
 apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
-sc.blit(apple.image, apple.rect)
 
 bushes = pg.sprite.Group()
 for i in range(count_troublse):
@@ -112,24 +117,29 @@ for i in range(count_troublse):
     Troubles((randint(0, W), (randint(0, H))), BUSHES_SURF[randint(0, 1)], bushes)
 
 bodes = pg.sprite.Group()
+body_elements = []
 
 pg.display.update()
 
 
 while 1:
     head = Head(x, y, 'head_snake.png', heads)
+    # if body_elements:
+    #     for i in body_elements:
+    #         Body(i[0], i[1], 'body.png', bodes)
+
 
     for i in pg.event.get():
         if i.type == pg.QUIT:
             sys.exit()
         elif i.type == pg.KEYDOWN:
-            if i.key == pg.K_LEFT:
+            if i.key == pg.K_LEFT and motion != RIGHT:
                 motion = LEFT
-            elif i.key == pg.K_RIGHT:
+            elif i.key == pg.K_RIGHT and motion != LEFT:
                 motion = RIGHT
-            elif i.key == pg.K_UP:
+            elif i.key == pg.K_UP and motion != DOWN:
                 motion = UP
-            elif i.key == pg.K_DOWN:
+            elif i.key == pg.K_DOWN and motion != UP:
                 motion = DOWN
 
     if motion == LEFT:
@@ -141,22 +151,35 @@ while 1:
     elif motion == DOWN:
         x, y = head.to_the_down()
 
-    bushes.draw(sc)
-    apples.draw(sc)
-    pg.display.update()
-
 
     for col in pg.sprite.groupcollide(heads, bushes, False, False).keys():
         sys.exit()
 
     for col in pg.sprite.groupcollide(heads, apples, True, True).keys():
         apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
-        sc.blit(apple.image, apple.rect)
 
-        print(head.x, head.y)
-        pos = head.x, head.y
-        body = Body(())
+        # x_b, y_b = head.x, head.y #змея не растет(скотина)
+        # body = Body(x_b, y_b, 'body.png', bodes)
+        # if motion == LEFT:
+        #     x_b, y_b = body.to_left()
+        #     x_b += 40
+        #
+        # elif motion == RIGHT:
+        #     head.x -= 40
+        # elif motion == UP:
+        #     head.y += 40
+        # elif motion == DOWN:
+        #     head.y -= 40
+        # print((x_b, y_b))
+        # body_elements.append((x_b, y_b))
 
+
+    bushes.draw(sc)
+    apples.draw(sc)
+    bodes.draw(sc)
+
+    pg.display.update()
+    pg.time.delay(10)
 
 
     pg.time.delay(30)
