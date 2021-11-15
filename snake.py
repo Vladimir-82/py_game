3e330678123e09d7a872111045c7dd75aae26300
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-11-15 15:37:55 +0300

    snake ok

--- snake.py
+++ snake.py
@@ -10,7 +10,6 @@ sc = pg.display.set_mode((W, H))
 # sc = pg.Surface((W, H))
 sc.fill(WHITE)
 
-
 RIGHT = "to the right"
 LEFT = "to the left"
 UP = "to the up"
@@ -20,58 +19,58 @@ motion = UP
 
 
 class Head(pg.sprite.Sprite):
+    speed = 2
 
-    def __init__(self, x, y, filename, speed, group):
+    def __init__(self, x, y, filename, motion, group):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(
             filename).convert_alpha()
         self.x = x
         self.y = y
-        self.speed = speed
+        self.motion = motion
         self.rect = self.image.get_rect(center=(self.x, self.y))
         self.add(group)
 
-
-    def to_the_left(self):
+    def move(self):
         sc.fill(WHITE)
-        rot = pg.transform.rotate(self.image, 90)
-        rot_rect = rot.get_rect(center=(self.x, self.y))
-        sc.blit(rot, rot_rect)
-        self.x -= self.speed
-        if self.x == 0:
-            self.x = W - self.rect.height//2
-        return self.x, self.y
+        if self.motion == LEFT:
+            rot = pg.transform.rotate(self.image, 90)
+            rot_rect = rot.get_rect(center=(self.x, self.y))
+            sc.blit(rot, rot_rect)
+            self.x -= Head.speed
+            if self.x < 0:
+                self.x = W
+            return self.x, self.y
 
-    def to_the_right(self):
-        sc.fill(WHITE)
-        rot = pg.transform.rotate(self.image, -90)
-        rot_rect = rot.get_rect(center=(self.x, self.y))
-        sc.blit(rot, rot_rect)
-        self.x += self.speed
-        if self.x == W:
-            self.x = self.rect.height//2
-        return self.x, self.y
+        elif motion == RIGHT:
+            rot = pg.transform.rotate(self.image, -90)
+            rot_rect = rot.get_rect(center=(self.x, self.y))
+            sc.blit(rot, rot_rect)
+            self.x += Head.speed
+            if self.x > W:
+                self.x = 0
+            return self.x, self.y
 
-    def to_the_up(self):
-        sc.fill(WHITE)
-        sc.blit(self.image, self.rect)
-        self.y -= self.speed
-        if self.y == 0:
-            self.y = H - self.rect.width//2
-        return self.x, self.y
 
-    def to_the_down(self):
-        sc.fill(WHITE)
-        rot = pg.transform.rotate(self.image, 180)
-        rot_rect = rot.get_rect(center=(self.x, self.y))
-        sc.blit(rot, rot_rect)
-        self.y += self.speed
-        if self.y == H:
-            self.y = self.rect.width//2
-        return self.x, self.y
+        elif motion == UP:
+            sc.blit(self.image, self.rect)
+            self.y -= Head.speed
+            if self.y < 0:
+                self.y = H
+            return self.x, self.y
+
+        elif motion == DOWN:
+            rot = pg.transform.rotate(self.image, 180)
+            rot_rect = rot.get_rect(center=(self.x, self.y))
+            sc.blit(rot, rot_rect)
+            self.y += Head.speed
+            if self.y > H:
+                self.y = 0
+            return self.x, self.y
 
 
 class Body(Head):
+
     def __init__(self, x, y, filename, group):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(filename).convert_alpha()
@@ -80,8 +79,8 @@ class Body(Head):
         self.add(group)
 
 
-
 class Apple(pg.sprite.Sprite):
+
     def __init__(self, x, y, surf, group):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(surf).convert_alpha()
@@ -97,8 +96,7 @@ class Troubles(pg.sprite.Sprite):
         self.add(group)
 
 
-x, y = W//2, H//2
-
+x, y = W // 2, H // 2
 
 BUSHES = ('bush_1.jpg', 'bush_2.jpg', 'bush_3.jpg')
 BUSHES_SURF = []
@@ -106,25 +104,29 @@ count_troublse = 5
 
 heads = pg.sprite.Group()
 
-apples = pg.sprite.Group()
-apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
-
 bushes = pg.sprite.Group()
 for i in range(count_troublse):
     BUSHES_SURF.append(pg.image.load(BUSHES[randint(0, 2)]).convert_alpha())
 
 for i in range(count_troublse):
-    Troubles((randint(0, W), (randint(0, H))), BUSHES_SURF[randint(0, 2)], bushes)
+    Troubles((randint(0, W // 2 - 80), (randint(0, H))), BUSHES_SURF[randint(0, 2)], bushes)
+    Troubles((randint(W // 2 + 80, W), (randint(0, H))), BUSHES_SURF[randint(0, 2)], bushes)
 
 bodes = pg.sprite.Group()
 
+apples = pg.sprite.Group()
+apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
+while pg.sprite.spritecollideany(apple, bushes):
+    apple.kill()
+    apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
+
 body_list = []
 lenght = 0
-speed = 3
 
 while 1:
     sc.fill(WHITE)
-    head = Head(x, y, 'head_snake.png', speed, heads)
+    head = Head(x, y, 'head_snake.png', motion, heads)
+    x, y = head.move()
 
     if lenght > 0:
 
@@ -134,8 +136,8 @@ while 1:
         for i in body_list[:-lenght]:
             i.kill()
 
-
     for i in pg.event.get():
+
         if i.type == pg.QUIT:
             sys.exit()
         elif i.type == pg.KEYDOWN:
@@ -148,40 +150,41 @@ while 1:
             elif i.key == pg.K_DOWN and motion != UP:
                 motion = DOWN
 
-    if motion == LEFT:
-        x, y = head.to_the_left()
-    elif motion == RIGHT:
-        x, y = head.to_the_right()
-    elif motion == UP:
-        x, y = head.to_the_up()
-    elif motion == DOWN:
-        x, y = head.to_the_down()
-
     for col in pg.sprite.groupcollide(heads, bushes, False, False).keys():
         sys.exit()
-    # for col in pg.sprite.groupcollide(heads, bodes, False, False).keys():
-    #     sys.exit()
+    if pg.sprite.spritecollideany(head, bodes):
+        bodes.draw(sc)
+
+        if motion == LEFT:
+            rot = pg.transform.rotate(head.image, 90)
+            rot_rect = rot.get_rect(center=(x, y))
+            sc.blit(rot, rot_rect)
+        elif motion == RIGHT:
+            rot = pg.transform.rotate(head.image, -90)
+            rot_rect = rot.get_rect(center=(x, y))
+            sc.blit(rot, rot_rect)
+        elif motion == UP:
+            sc.blit(head.image, head.rect)
+        elif motion == DOWN:
+            rot = pg.transform.rotate(head.image, 180)
+            rot_rect = rot.get_rect(center=(x, y))
+            sc.blit(rot, rot_rect)
+
 
 
     for col in pg.sprite.groupcollide(heads, apples, False, True).keys():
+
         lenght += 15
-        speed += 0.2
+        Head.speed += 0.1
 
         apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
         while pg.sprite.spritecollideany(apple, bushes):
             apple.kill()
             apple = Apple((randint(0, W)), (randint(0, H)), 'apple.png', apples)
 
-
-
-
     bushes.draw(sc)
     apples.draw(sc)
-    bodes.draw(sc)
-    # heads.draw(sc)
 
     pg.display.update()
 
-
     pg.time.delay(30)
-
