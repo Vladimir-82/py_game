64cb3b246d8a6f76375ef0946499d0fcb049b21f
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-11-17 14:08:22 +0300

    mashrooms q

--- snake.py
+++ snake.py
@@ -6,6 +6,9 @@ pg.init()
 W, H = 600, 600
 WHITE = (255, 255, 255)
 MAIN_FONT = (100, 150, 200)
+
+clock = pg.time.Clock()
+FPS = 30
 sc = pg.display.set_mode((W, H))
 # sc = pg.Surface((W, H))
 sc.fill(WHITE)
@@ -16,7 +19,10 @@ UP = "to the up"
 DOWN = "to the down"
 
 motion = UP
+mashroom = None
+raund = 0
 
+x, y = W // 2, H // 2
 
 class Head(pg.sprite.Sprite):
     speed = 2
@@ -87,6 +93,14 @@ class Fruit(pg.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x, y))
         self.add(group)
 
+class Mashroom(pg.sprite.Sprite):
+
+    def __init__(self, x, y, surf, group):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load(surf).convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+        self.add(group)
+
 
 class Troubles(pg.sprite.Sprite):
     def __init__(self, pos, surf, group):
@@ -96,8 +110,7 @@ class Troubles(pg.sprite.Sprite):
         self.add(group)
 
 heads = pg.sprite.Group()
-
-x, y = W // 2, H // 2
+head = Head(x, y, 'head_snake.png', motion, heads)
 
 BUSHES = ('bush_1.jpg', 'bush_2.jpg', 'bush_3.jpg')
 BUSHES_SURF = []
@@ -108,17 +121,21 @@ for i in range(count_troublse):
     BUSHES_SURF.append(pg.image.load(BUSHES[randint(0, 2)]).convert_alpha())
 
 for i in range(count_troublse):
-    Troubles((randint(0, W // 2 - 80), (randint(0, H))), BUSHES_SURF[randint(0, 2)], bushes)
-    Troubles((randint(W // 2 + 80, W), (randint(0, H))), BUSHES_SURF[randint(0, 2)], bushes)
+    Troubles((randint(0, W // 2 - 2 * head.image.get_width()), (randint(0, H))),
+             BUSHES_SURF[randint(0, 2)], bushes)
+    Troubles((randint(W // 2 + 2 * head.image.get_width(), W), (randint(0, H))),
+             BUSHES_SURF[randint(0, 2)], bushes)
 
 FRUITS = ('apple.png', 'banana.jpg', 'pineaplle.jpg')
 fruits = pg.sprite.Group()
-fruit = Fruit((randint(0, W)), (randint(0, H)), FRUITS[randint(0, 2)], fruits)
+fruit = Fruit((randint(head.image.get_width()//2, W - head.image.get_width()//2)),
+              (randint(head.image.get_width()//2, H - head.image.get_width()//2)), FRUITS[randint(0, 2)], fruits)
 
 while pg.sprite.spritecollideany(fruit, bushes):
     fruit.kill()
     fruit = Fruit((randint(0, W)), (randint(0, H)), FRUITS[randint(0, 2)], fruits)
 
+mashrooms = pg.sprite.Group()
 
 bodes = pg.sprite.Group()
 body_list = []
@@ -150,7 +167,14 @@ while 1:
                 motion = DOWN
 
     for col in pg.sprite.groupcollide(heads, bushes, False, False).keys():  # snake eats bushes
+        print('snake eats bushes')
         sys.exit()
+    for col in pg.sprite.groupcollide(heads, mashrooms, False, False).keys():  # snake eats mashroom
+        print('snake eats mashroom')
+        sys.exit()
+    for col in pg.sprite.groupcollide(fruits, mashrooms, True, False).keys():  # snake eats mashroom
+        print('mashroom kills fruit')
+        fruit = Fruit((randint(0, W)), (randint(0, H)), FRUITS[randint(0, 2)], fruits)
 
     if pg.sprite.spritecollideany(head, bodes):
         bodes.draw(sc)
@@ -171,14 +195,14 @@ while 1:
             sc.blit(rot, rot_rect)
 
     for col in pg.sprite.groupcollide(heads, fruits, False, True).keys():
-        if lenght < 40:
-            lenght += 20
-        elif 40 <= lenght <= 80:
-            lenght += 10
-        elif 81 <= lenght <= 1000:
-            lenght += 5
 
-        Head.speed += 0.1
+        FPS += 2
+        if lenght < head.image.get_width():
+            lenght += head.image.get_width()//2
+        elif head.image.get_width() <= lenght <= head.image.get_width() * 2:
+            lenght += head.image.get_width()//4
+        elif 2 * head.image.get_width() + 1 <= lenght <= 100 * head.image.get_width():
+            lenght += head.image.get_width()//8
 
         fruit = Fruit((randint(0, W)), (randint(0, H)), FRUITS[randint(0, 2)], fruits)
 
@@ -186,12 +210,26 @@ while 1:
             fruit.kill()
             fruit = Fruit((randint(0, W)), (randint(0, H)), FRUITS[randint(0, 2)], fruits)
 
+    if not mashroom:
+        dice = randint(1, 500)
+        if dice == 1:
+            mashroom = Mashroom((randint(head.image.get_width() // 2, W - head.image.get_width() // 2)),
+                                (randint(head.image.get_width() // 2, H - head.image.get_width() // 2)), 'mashroom.jpg',
+                                mashrooms)
+            print('mashroom growns')
+            raund = 0
+    if mashroom:
+        if raund == 500:
+            mashroom.kill()
+            mashroom = None
+
     if len(pg.sprite.spritecollide(head, bodes, False)) > 50:  # snake eats itself
         print('snake eats itself')
         sys.exit()
 
     bushes.draw(sc)
     fruits.draw(sc)
+    mashrooms.draw(sc)
     pg.display.update()
-
-    pg.time.delay(30)
+    raund += 1
+    clock.tick(FPS)
